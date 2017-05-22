97bb32d407697d5da16ca6939c7f9f76c8e96e53
捡漏脚本 <18792544700@163.com>
2017-05-22 13:28:09 +0800

    Update posturl.py

--- posturl.py
+++ posturl.py
@@ -1,7 +1,8 @@
 #-*- coding: utf-8 -*- 
-import requests
-
-cookies = 'your cookie'#https://shop.48.cn/tickets/item/1006 的cookie
+import requests,urllib
+urllib.getproxies_registry = lambda : {}
+cookies = 'your cookie'
+#https://shop.48.cn/tickets/item/* 的cookie，×为门票编号
 
 postheader = {'Accept':'application/json, text/javascript, */*; q=0.01',
            'Accept-Encoding':'gzip, deflate, sdch, br',
@@ -12,17 +13,23 @@ postheader = {'Accept':'application/json, text/javascript, */*; q=0.01',
            'Cookie':cookies,
            'Host':'shop.48.cn',
            'Origin':'https://shop.48.cn',
-           'Referer':'https://shop.48.cn/tickets/item/1006',#1006为门票编号
+           'Referer':'https://shop.48.cn/tickets/item/*',#*为门票编号
            'Referer':'https://shop.48.cn/tickets',
            'Upgrade-Insecure-Requests':'1',
            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
            'X-Requested-With':'XMLHttpRequest'}
 
-postData = {'id':'1006','num':'1','seattype':'3','brand_id':'3','r':'0.3731131006391708'}
-#id:门票编号，num:门票数量，seattype:门票类型2为VIP，3为普座，4为站票，brand_id：未知，’r‘:随机数
+postData = {'id':'×','num':'1','seattype':'2','brand_id':'3','r':'0.3731131006391708'}
+#id:门票编号，num:门票数量，seattype:门票类型,2为VIP，3为普座，4为站票，brand_id：团体编号(gnz48为3)，’r‘:随机数
 if __name__ == '__main__':
     r = requests.session()
-    content = r.post('https://shop.48.cn/TOrder/add',headers = postheader,data = postData)
-    print content.status_code
-
+    res = r.get('https://shop.48.cn/tickets/item/994',headers = {'Cookie':cookies})
+    while 1:
+        content = r.get('https://shop.48.cn/tickets/saleList?id=*&brand_id=*')#查询库存，*为门票编号和库存
+        if eval(content.text.replace('true','""'))[*]['amount'] == 1:#*为票种，1为VIP，2为普座，3为站票
+            content = r.post('https://shop.48.cn/TOrder/add',headers = postheader,data = postData)
+            if content.status_code == 200:
+                print '下单成功，请前往shop.snh48.com付款。'
+                break
+        else:continue
 
