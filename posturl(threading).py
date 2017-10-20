dea5d229254fb2b8a85eb8faf25658747f82fe8e
北斗第一星 <18792544700@163.com>
2017-10-21 06:22:26 +0800

    Update posturl(threading).py

--- posturl(threading).py
+++ posturl(threading).py
@@ -1,17 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from tkinter import *
 import time
 import requests
-import urllib
 import traceback
 import threading
 import json
 
-
-class order(object):
+class ORDER(object):
     def __init__(self,user,password,ticketcode,seattype):
         self.user = user
         self.password = password
@@ -19,7 +15,6 @@ class order(object):
         self.seattype = seattype
         self.cookies = ''
         self.req = requests.session()
-        root = Tk()
         self.login()
 
     def login(self):
@@ -47,21 +42,23 @@ class order(object):
     	url = 'http://shop.48.cn'
     	res = self.req.get(url,headers = {'Cookie':self.cookies})
         postData = {'id': self.ticketcode, 'num': '1', 'seattype': self.seattype,'brand_id': '3', 'r': '0.3731131006391708'}  # id:门票编号，num:门票数量，seattype:门票类型,2为VIP，3为普座，4为站票，brand_id：团体编号(gnz48为3)，’r‘:随机数
+        url = 'https://shop.48.cn/tickets/saleList?id={}&brand_id=3'.format(self.ticketcode)
+        types = int(self.seattype) - 1
         while 1:
 	        try:
 	            res = self.req.get(url)
 	        except:
 	            traceback.print_exc()
 	            continue
-	        if json.loads(res.content)[1]['amount']:  # *为票种，1为VIP，2为普座，3为站票
+	        if json.loads(res.content)[types]['amount']:
 	            resp = self.req.post('https://shop.48.cn/TOrder/add',headers={'Cookie': self.cookies}, data=postData)
 	            if resp.status_code == 200:
-	                print '下单成功，请前往shop.snh48.com付款。'
+	                print 'add succeed...'
 	        else:
 	            continue
 
 if __name__ == '__main__':
-	se = order('snh48042191478','yinheng1993','1426','2')  # id:门票编号，seattype:门票类型,2为VIP，3为普座，4为站票
+	se = ORDER('username','password','ticketcode','seattype')  # id:门票编号，seattype:门票类型,2为VIP，3为普座，4为站票
 	for i in range(30):
-        th = threading.Thread(target=se.ticket)
-        th.start()
+	    th = threading.Thread(target=se.ticket)
+	    th.start()
